2a16ed42fc46ea40f591a52989a7fb009b06a672
sokayacou <sokandayacouba233@gmail.com>
2026-05-30 11:00:17 +0000

    Configuration de deploiement

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/projet-next',
 }
 
 module.exports = nextConfig
